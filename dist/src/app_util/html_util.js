db705c6939f220ac7a4e6a10b725d7b3a10ba068
Kyle Murphy <kmurph73@gmail.com>
2021-12-18 14:41:59 -0800

    huh

--- dist/src/app_util/html_util.js
+++
@@ -1,7 +0,0 @@
-import { html } from "../constants.js";
-export const setDisabled = (elements, disabled) => {
-    for (let index = 0; index < elements.length; index++) {
-        const element = html[elements[index]];
-        element.disabled = disabled;
-    }
-};
