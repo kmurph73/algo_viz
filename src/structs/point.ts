f2eb16e792d083c5d9b5603ae0fd54c4e894aa94
Kyle Murphy <kmurph73@gmail.com>
2021-12-19 08:26:43 -0800

    hm

--- src/structs/point.ts
+++
@@ -1,9 +0,0 @@
-export type Point = { x: number; y: number };
-
-export const pointsEq = (p1: Point, p2: Point): boolean => {
-  return p1.x === p2.x && p1.y === p2.y;
-};
-
-export const addPoints = (p1: Point, p2: Point): Point => {
-  return { x: p1.x + p2.x, y: p1.y + p2.y };
-};
